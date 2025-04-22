8d23033eadfeca7ae3ecd8cefe0840d4227b9e57
Nancy Butler <42977925+mantis-toboggan-md@users.noreply.github.com>
2025-04-22 14:54:47 -0700

    wip install

--- pkg/capi/edit/cluster.x-k8s.io.cluster/ClusterConfig.vue
+++ pkg/capi/edit/cluster.x-k8s.io.cluster/ClusterConfig.vue
@@ -275,6 +275,11 @@ export default {
         out.push(subtype);
       }
     },
+
+    // TODO nb not this
+    canCreateGitRepos() {
+      return true;
+    }
   },
   methods: {
     set,
@@ -375,6 +380,10 @@ export default {
       } else {
         delete this.value.metadata.labels['cluster-api.cattle.io/rancher-auto-import'];
       }
+    },
+
+    openRepoModal() {
+      this.$store.dispatch('management/promptModal', { component: 'AddExampleRepoDialog', modalWidth: '800px' });
     }
   }
 };
@@ -406,7 +415,24 @@ export default {
         name-field="label"
         side-label-field="tag"
         @clicked="clickedType"
-      />
+      >
+        <template #no-rows>
+          <div v-if="canCreateGitRepos">
+            It looks like you don't have any cluster classes. Rancher Turtles has a curated collection of sample classes to get you started.
+            <div class="mt-20">
+              <button
+                type="button"
+                class="btn role-secondary"
+                @click="openRepoModal"
+              >
+                Add Example Classes
+              </button>
+            </div>
+          </div>
+        </template>
+        <CardGrid>
+        </cardgrid>
+      </cardgrid>
     </template>
     <template #stepConfiguration>
       <NameNsDescription
